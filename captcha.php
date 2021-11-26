d35714c5ae2ccd1bd0a46e8c28e765ac927a6688
Skyrim999 <95053661+Skyrim999@users.noreply.github.com>
2021-11-26 14:01:09 -0300

    Create captcha.php

---
+++ captcha.php
@@ -0,0 +1,10 @@
+<title>reCAPTCHA demo: Simple page</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  </head>
+  <body>
+    <form action="?" method="POST">
+      <div class="g-recaptcha" data-sitekey="your_site_key"></div>
+      <br/>
+      <input type="submit" value="Submit">
+
+6LdO514dAAAAAJkk7qv9sD0pbrT8SQgUr4bTrPoi
